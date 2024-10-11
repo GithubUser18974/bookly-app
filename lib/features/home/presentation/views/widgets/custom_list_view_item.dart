08d14d8360bdf1be254a1e8541f2966ebdabd6f1
Github User <mohamedaraby1296@gmail.com>
2024-10-12 00:51:50 +0300

    add featured list view to HOME

--- lib/features/home/presentation/views/widgets/custom_list_view_item.dart
+++ lib/features/home/presentation/views/widgets/custom_list_view_item.dart
@@ -7,19 +7,16 @@ class CustomListViewItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * .3,
-      child: AspectRatio(
-        aspectRatio: 2.7 / 4,
-        child: Container(
-          width: MediaQuery.of(context).size.width * 0.2,
-          height: MediaQuery.of(context).size.height * .25,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(16),
-            color: Colors.red,
-            image: DecorationImage(
-                image: AssetImage(AssetsData.testImage), fit: BoxFit.fill),
-          ),
+    return AspectRatio(
+      aspectRatio: 2.7 / 4,
+      child: Container(
+        width: MediaQuery.of(context).size.width * 0.2,
+        height: MediaQuery.of(context).size.height * .25,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          color: Colors.red,
+          image: DecorationImage(
+              image: AssetImage(AssetsData.testImage), fit: BoxFit.fill),
         ),
       ),
     );
