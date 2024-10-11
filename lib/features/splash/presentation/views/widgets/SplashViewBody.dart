ef30f9c65395c8dbdbcb11ddf2a7e10d8972d799
Github User <mohamedaraby1296@gmail.com>
2024-10-11 03:11:54 +0300

    adding sliding animation to splash screen text

--- lib/features/splash/presentation/views/widgets/SplashViewBody.dart
+++ lib/features/splash/presentation/views/widgets/SplashViewBody.dart
@@ -1,8 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:untitled/core/utils/assets.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 5));
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 2), end: Offset.zero)
+            .animate(animationController);
+    animationController.forward();
+    slidingAnimation.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    animationController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,10 +41,15 @@ class SplashViewBody extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Image.asset(AssetsData.logo),
-        const SizedBox(height: 5,),
-        const Center(
-          child: Text(
-            'Read To Learn',
+        const SizedBox(
+          height: 3,
+        ),
+        Center(
+          child: SlideTransition(
+            position: slidingAnimation,
+            child: const Text(
+              'Read To Learn',
+            ),
           ),
         ),
       ],
