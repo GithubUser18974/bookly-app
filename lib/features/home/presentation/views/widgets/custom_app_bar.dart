e5051ecafe68a04f8803831f9df0807e9991fef5
Github User <mohamedaraby1296@gmail.com>
2024-10-13 01:35:17 +0300

    search view done

--- lib/features/home/presentation/views/widgets/custom_app_bar.dart
+++ lib/features/home/presentation/views/widgets/custom_app_bar.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:untitled/core/utils/app_router.dart';
 
 import '../../../../../core/utils/assets.dart';
 
@@ -17,7 +19,9 @@ class CustomAppBar extends StatelessWidget {
           ),
           const Spacer(),
           IconButton(
-              onPressed: () {},
+              onPressed: () {
+                GoRouter.of(context).push(AppRouter.kSearchView);
+              },
               icon: const Icon(
                 Icons.search,
                 color: Colors.white,
--- lib/features/search/presentation/views/widgets/search_view_body.dart
+++ lib/features/search/presentation/views/widgets/search_view_body.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:untitled/features/search/presentation/views/widgets/search_result_list_view.dart';
+
+import 'custom_search_text_field.dart';
 
 class SearchViewBody extends StatelessWidget {
   const SearchViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      child: Column(
+        children: [
+          CustomSearchTextField(),
+          SizedBox(
+            height: 20,
+          ),
+          Expanded(child: SearchResultListView()),
+        ],
+      ),
+    );
   }
 }
