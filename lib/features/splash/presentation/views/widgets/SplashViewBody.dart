379b7d2f156a4c300577138ddc8e094dcf8736d4
Github User <mohamedaraby1296@gmail.com>
2024-10-12 05:26:27 +0300

    refactor navigation in our app

--- lib/features/splash/presentation/views/widgets/SplashViewBody.dart
+++ lib/features/splash/presentation/views/widgets/SplashViewBody.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:go_router/go_router.dart';
+import 'package:untitled/core/utils/app_router.dart';
 import 'package:untitled/core/utils/assets.dart';
-import 'package:untitled/features/home/presentation/views/home_view.dart';
-
-import '../../../../../core/constants.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -30,8 +28,7 @@ class _SplashViewBodyState extends State<SplashViewBody>
 
   void navigateToHomeView() {
     Future.delayed(const Duration(seconds: 2), () {
-      Get.to(() => const HomeView(),
-          transition: Transition.fade, duration: kTransitionDuration);
+      GoRouter.of(context).push(AppRouter.kHomeView);
     });
   }
 
