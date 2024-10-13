3e4ca5a2e2bd54fe70f10c0328bce504bb6ffc25
Github User <mohamedaraby1296@gmail.com>
2024-10-13 04:27:51 +0300

    create Api wrapper servcice

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,4 +1,10 @@
+import 'package:dartz/dartz.dart';
+import 'package:untitled/core/errors/failure.dart';
+import 'package:untitled/features/home/data/models/book_model/book_model.dart';
+
 abstract class HomeRepo {
-  fetchBestSellerBooks();
-  fetchFeaturedBooks();
+ Future<Either<Failure,List<BookModel>>> 
+ fetchBestSellerBooks();
+ Future<Either<Failure,List<BookModel>>>
+ fetchFeaturedBooks();
 }
