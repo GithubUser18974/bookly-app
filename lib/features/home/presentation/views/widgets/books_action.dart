56bf9ee0422ca2690f6f4d04339b51d77b6126ad
Github User <mohamedaraby1296@gmail.com>
2024-10-14 09:36:49 +0300

    Finished

--- lib/features/home/presentation/views/widgets/books_action.dart
+++ lib/features/home/presentation/views/widgets/books_action.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:untitled/features/home/data/models/book_model/book_model.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../../../../../core/widgets/custome_button.dart';
 
 class BooksAction extends StatelessWidget {
-  const BooksAction({super.key});
-
+  const BooksAction({super.key, required this.book});
+  final BookModel book;
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -24,6 +26,12 @@ class BooksAction extends StatelessWidget {
           ),
           Expanded(
             child: CustomeButton(
+              onPressed: () async {
+                Uri uri = Uri.parse(book.volumeInfo!.previewLink!);
+                if (!await launchUrl(uri)) {
+                  throw Exception('Could not launch $uri');
+                }
+              },
               fontSize: 16,
               text: 'Free Preview',
               backgroundColor: Color(0xffef8262),
